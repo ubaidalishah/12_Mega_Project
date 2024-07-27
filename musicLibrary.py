e80fdca078a767f05b7b6a8e803d81d6cadbab09
ubaidalishah <ubaidalishah18@gmail.com>
2024-07-27 13:54:32 +0500

    adding comment to music library

--- musicLibrary.py
+++ musicLibrary.py
@@ -3,3 +3,4 @@ music={
     "nida":"https://youtu.be/pq7vA3ywGCQ?si=Yy0P4gc664HL0zCL",
     "rose":"https://youtu.be/G1gsK6m178I?si=kPL1uP1_tsXUObc4"
 }
+#this is the dictonary of music where you add your music the key is what the jarvis would want to listen and then the value would be pasted according to your logic 
